b84e69ee9724ade2e47b4e07dbc87c172200c1d4
Georgi Kostov <p_e_a@gbg.bg>
2012-06-26 16:13:32 +0300

    renamed util.js to helpers.js and using the file util.js as a browser module replicating parts of node's util.js

--- client/lib/util.js
+++ client/lib/util.js
@@ -1,78 +1,35 @@
-
+// if registerModule is defined then we must be in the browser so call that. if not then this has
+// been loaded as a node.js module and the code can execute right away.
+(typeof registerModule=='function' ? registerModule : function (fn){fn(module);}).call(this, function (module){
+	var exports=module.exports,
+		require=module.require;
+		
+// copied over from node's util.js
 /**
- * Helper prototype additions.
- * @version 0.1.0
+ * Inherit the prototype methods from one constructor into another.
+ *
+ * The Function.prototype.inherits from lang.js rewritten as a standalone
+ * function (not on Function.prototype). NOTE: If this file is to be loaded
+ * during bootstrapping this function needs to be revritten using some native
+ * functions as prototype setup using normal JavaScript does not work as
+ * expected during bootstrapping (see mirror.js in r114903).
+ *
+ * @param {function} ctor Constructor function which needs to inherit the
+ *     prototype.
+ * @param {function} superCtor Constructor function to inherit prototype from.
  */
-
-Function.prototype.scope=function (scopeObj){
-	var f=this;	
-	return function (){
-		return f.apply(scopeObj, arguments);
-	};
-};
-Function.prototype.createCallback=function (scopeObj, params, appendArgs){
-	var f=this;	
-	return function (){
-		var args=params||arguments;
-		if(appendArgs === true)
-			args=[].slice.call(arguments, 0).concat(args);
-		return f.apply(scopeObj||this, args);
-	};
-};
-/**
- * Creates a function which adds an intermediate function which is to receive the result returned by the original and the caller will
- * receive the result returned by the interceptor.
- */
-Function.prototype.createResultInterceptor=function (interceptorFn, scopeObj){
-	var f=this;	
-	return function (){
-		return interceptorFn.call(scopeObj||this, f.apply(this, arguments));
-	};
-};
-/**
- * Creates a function which adds an intermediate function which will be called instead of the original. The interceptor will receive
- * the original function, the arguments sent by the caller and the scope object. The result returned by the interceptor will be sent back to the caller. This way
- * the interceptor function has full control over what arguments are passed to the original (or decide to not call it at all) and what gets sent back to the caller.
- */
-Function.prototype.createCallInterceptor=function (interceptorFn, scopeObj){
-	var f=this;	
-	return function (){
-		return interceptorFn.call(scopeObj||this, f, [].slice.call(arguments, 0), this);
-	};
-};
-/**
- * This is much like createCallInterceptor but the interceptor is only given a chance to inspect and alter the arguments sent by the caller which are then passed on to the callee.
- */
-Function.prototype.createArgumentsInterceptor=function (interceptorFn, scopeObj){
-	var f=this;	
-	return function (){
-		var args=[].slice.call(arguments, 0);
-		interceptorFn.call(scopeObj||this, args);
-		return f.apply(this, args);
-	};
-};
-/**
- * This is much like createArgumentsInterceptor but the interceptor is not given the chance to alter the arguments sent by the caller. It is simply called
- * after the original and before the result is returned to the caller.
- */
-Function.prototype.chain=function (func, scopeObj){
-	var f=this;
-	return function (){
-		var retval = f.apply(this, arguments);
-		func.apply(scopeObj||this, arguments);
-		return retval;
-	};
-};
-Function.prototype.defer=function (millis, scopeObj, args){
-	var f=this;	
-	return setTimeout(function (){
-		f.apply(scopeObj||window, args||[]);
-	}, millis);
+exports.inherits = function(ctor, superCtor) {
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
 };
 
-Date.prototype.clearTime=function (){
-	this.setHours(0, 0, 0, 0);
-	return this;
-};
+});
 
 
