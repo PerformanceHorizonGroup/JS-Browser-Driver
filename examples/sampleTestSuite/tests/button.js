065de8732276f6020ff71e1f8a0282502490f671
Georgi Kostov <p_e_a@gbg.bg>
2012-11-26 14:26:15 +0200

    updated to use the new architecture

--- examples/sampleTestSuite/tests/button.js
+++ examples/sampleTestSuite/tests/button.js
@@ -1,102 +1,124 @@
-
-//driver.loadLib("util");
-//driver.loadLib("recordPageEvents/replayPageEvents");
+module('jQueryUI', {
+	setup:function (){
+		console.log('module setup for '+this.__testInst.name);
+		var env=this;
+		this.__testInst.requireCustomLib(['util', 'recordPageEvents/replayPageEvents', 'frameMngr'], function (exportsList){
+			env.testUtils=exportsList[0];
+			var waitFn=env.__testInst.waitForSetup();
+			env.frameMngr=exportsList[2].createFrame(function (frameMngr){
+				frameMngr.el.css({border:0, width:'100%', height:'100%'});
+				env.player=exportsList[1].createPlayer(frameMngr, function (player){
+					env.player=player;
+					waitFn();
+				});
+			});
+		});
+	},
+	teardown:function (){
+		console.log('module teardown for '+this.__testInst.name);
+		this.player.frameMngr.el.remove();
+		this.player.destroy();
+	}
+});
 asyncTest("Button widget", 37, function (){
-	var test=this;
+	var test=this,
+		p=this.player,
+		testUtils=this.testUtils,
+		frameEl=p.frameMngr.el;
 
-	driver.storage.ReplayPageEvents.initialize();
+	p.initialize();
 
 	function testIsTheOnlyElementWithClass(selector, cls){
-		var sel=testUtils.$('.'+cls); 
+		var sel=testUtils.$(frameEl, '.'+cls); 
 		return sel.length==1 && sel.is(selector);
 	}
 
-	testUtils.setPath('/public_html/jqueryui/index.html', ok); // load the index page
-	testWaitForEvent("load", driver.targetSiteFrame);
-	wait(800);
-	testFireEvent("click", "html>body>ul>li:eq(2)>a");
-	testWaitForEvent("load", driver.targetSiteFrame);
-	setAutoWait(500); // set 1 sec delay before each scheduled call
-	testFireEvent("click", "button#create");
+	testUtils.setPath(frameEl, '/public_html/jqueryui/index.html', ok); // load the index page
+	p.testWaitForEvent("load", frameEl);
+	p.wait(800);
+	p.testFireEvent("click", "html>body>ul>li:eq(2)>a");
+	p.testWaitForEvent("load", frameEl);
+	p.setAutoWait(500); // set 1 sec delay before each scheduled call
+	p.testFireEvent("click", "button#create");
 
 	var selector="html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only>span.ui-button-text";
-	testFireEvent("mouseover", selector);
-	execCb(function (){
+	p.testFireEvent("mouseover", selector);
+	p.execCb(function (){
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", 'ui-state-hover'), "the button is the only one with class ui-state-hover");
 	});
-	testFireEvent("mousedown", selector);
-	execCb(function (){
+	p.testFireEvent("mousedown", selector);
+	p.execCb(function (){
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", 'ui-state-hover'), "the button is the only one with class ui-state-hover");
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", 'ui-state-active'), "the button is the only one with class ui-state-active");
 	});
-	testFireEvent("mouseup", selector);
-	execCb(function (){
+	p.testFireEvent("mouseup", selector);
+	p.execCb(function (){
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", 'ui-state-hover'), "the button is the only one with class ui-state-hover");
-		ok(!testUtils.$("html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only").hasClass('ui-state-active'), 'the button does not have class ui-state-active');
+		ok(!testUtils.$(frameEl, "html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only").hasClass('ui-state-active'), 'the button does not have class ui-state-active');
 	});
-	testFireEvent("mouseout", selector);
+	p.testFireEvent("mouseout", selector);
 	
 	selector="html>body>div.demo>input.ui-button.ui-widget.ui-state-default.ui-corner-all";
-	testFireEvent("mouseover", selector);
-	execCb(function (){
+	p.testFireEvent("mouseover", selector);
+	p.execCb(function (){
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>input.ui-button.ui-widget.ui-state-default.ui-corner-all", 'ui-state-hover'), "the button is the only one with class ui-state-hover");
 	});
-	testFireEvent("mousedown", selector);
-	execCb(function (){
+	p.testFireEvent("mousedown", selector);
+	p.execCb(function (){
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>input.ui-button.ui-widget.ui-state-default.ui-corner-all", 'ui-state-hover'), "the button is the only one with class ui-state-hover");
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>input.ui-button.ui-widget.ui-state-default.ui-corner-all", 'ui-state-active'), "the button is the only one with class ui-state-active");
 	});
-	testFireEvent("mouseup", selector);
-	execCb(function (){
+	p.testFireEvent("mouseup", selector);
+	p.execCb(function (){
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>input.ui-button.ui-widget.ui-state-default.ui-corner-all", 'ui-state-hover'), "the button is the only one with class ui-state-hover");
-		ok(!testUtils.$("html>body>div.demo>input.ui-button.ui-widget.ui-state-default.ui-corner-all").hasClass('ui-state-active'), 'the button does not have class ui-state-active');
+		ok(!testUtils.$(frameEl, "html>body>div.demo>input.ui-button.ui-widget.ui-state-default.ui-corner-all").hasClass('ui-state-active'), 'the button does not have class ui-state-active');
 	});
-	testFireEvent("mouseout", selector);
+	p.testFireEvent("mouseout", selector);
 	
 	selector="html>body>div.demo>a.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only>span.ui-button-text";
-	testFireEvent("mouseover", selector);
-	execCb(function (){
+	p.testFireEvent("mouseover", selector);
+	p.execCb(function (){
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>a.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", 'ui-state-hover'), "the button is the only one with class ui-state-hover");
 	});
-	testFireEvent("mousedown", selector);
-	execCb(function (){
+	p.testFireEvent("mousedown", selector);
+	p.execCb(function (){
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>a.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", 'ui-state-hover'), "the button is the only one with class ui-state-hover");
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>a.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", 'ui-state-active'), "the button is the only one with class ui-state-active");
 	});
-	testFireEvent("mouseup", selector);
-	execCb(function (){
+	p.testFireEvent("mouseup", selector);
+	p.execCb(function (){
 		ok(testIsTheOnlyElementWithClass("html>body>div.demo>a.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", 'ui-state-hover'), "the button is the only one with class ui-state-hover");
-		ok(!testUtils.$("html>body>div.demo>a.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only").hasClass('ui-state-active'), 'the button does not have class ui-state-active');
+		ok(!testUtils.$(frameEl, "html>body>div.demo>a.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only").hasClass('ui-state-active'), 'the button does not have class ui-state-active');
 	});
-	testFireEvent("mouseout", selector);
-	execCb(function (){
-		ok(testUtils.$('.ui-state-hover, .ui-state-active').length==0, 'there are no buttons with class ui-state-hover or ui-state-active');
+	p.testFireEvent("mouseout", selector);
+	p.execCb(function (){
+		ok(testUtils.$(frameEl, '.ui-state-hover, .ui-state-active').length==0, 'there are no buttons with class ui-state-hover or ui-state-active');
 	});
 	
 	// try disabling a button while it is being hovered over ( bug #5295 )
 	var selector="html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only>span.ui-button-text";
 	// move the mouse over it
-	testFireEvent("mouseover", selector);
+	p.testFireEvent("mouseover", selector);
 	// disable it
-	execCb(function (){
-		driver.targetSiteFrame.get(0).contentWindow
-			.$("html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", driver.targetSiteFrame.get(0).contentWindow.document)
+	p.execCb(function (){
+		frameEl.get(0).contentWindow
+			.$("html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", frameEl.get(0).contentWindow.document)
 			.button('disable');
 	});
 	// move the mouse away
-	testFireEvent("mouseout", selector);
+	p.testFireEvent("mouseout", selector);
 	// enable it
-	execCb(function (){
-		driver.targetSiteFrame.get(0).contentWindow
-			.$("html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", driver.targetSiteFrame.get(0).contentWindow.document)
+	p.execCb(function (){
+		frameEl.get(0).contentWindow
+			.$("html>body>div.demo>button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only", frameEl.get(0).contentWindow.document)
 			.button('enable');
 	});
 	// it is not expected to have the ui-state-hover class but in 1.8.x it will ;-)
-	execCb(function (){
-		ok(testUtils.$('.ui-state-hover, .ui-state-active').length==0, 'there are no buttons with class ui-state-hover or ui-state-active');
+	p.execCb(function (){
+		ok(testUtils.$(frameEl, '.ui-state-hover, .ui-state-active').length==0, 'there are no buttons with class ui-state-hover or ui-state-active');
 	});
 	
-	execCb(function (){
+	p.execCb(function (){
 		ok(true, "complete");
 		start();
 	});
--- examples/sampleTestSuite/tests/recordPageEvents.js
+++ examples/sampleTestSuite/tests/recordPageEvents.js
@@ -1,19 +1,33 @@
 (function (){
-//	driver.loadLib('recordPageEvents/recordPageEvents');
-	
-	module('Record page events');
-	/**
-	 * TO-DO: need to implement creation and destruction of the control bar as module lifecycle procedures
-	 */
+	module('Record page events', {
+		setup:function (){
+			console.log('module setup for '+this.__testInst.name);
+			var env=this;
+			this.__testInst.requireCustomLib(['recordPageEvents/recordPageEvents', 'frameMngr'], function (exportsList){
+				env.RecordPageEvents=exportsList[0];
+//				env.RecordPageEvents.initialize();
+				env.frameMngr=exportsList[1];
+			});
+		},
+		teardown:function (){
+			console.log('module teardown for '+this.__testInst.name);
+		}
+	});
 	
 	asyncTest('Record page events', function (){
-		driver.storage.RecordPageEvents.initialize();
-		driver.storage.RecordPageEvents.getControlPanel(); // create the panel
-		driver.storage.RecordPageEvents.bind('beforeClose', function (obj){
-			ok(true);
-			start();
+		var env=this;
+		this.frameMngr.createFrame(function (frameMngr){
+			frameMngr.el.css({border:0, width:'100%', height:'100%'});
+			env.RecordPageEvents.createControlPanel(frameMngr, function (panel){
+				panel.on('beforeClose', function (obj){
+					ok(true);
+					start();
+				});
+				panel.on('destroy', function (obj){
+					frameMngr.el.remove();
+				});
+			});
 		});
-		
 	});
 	
 }());
